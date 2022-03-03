40833362c691bac0642c9d8e03e5d37cdfbd9a1b
KhalidStonehouse2001 <khalidstonehouse12@outlook.com>
2022-03-03 15:59:45 +0000

    changes

--- src/components/CreateJob.jsx
+++ src/components/CreateJob.jsx
@@ -1,185 +1,185 @@
-import Nav from "./Nav";
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { useState, useEffect } from "react";
+import Nav from './Nav';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 import {
-  getSkills,
-  postNewTask,
-  getSkillsSubCat,
-} from "../Utils/api-createJob";
+	getSkills,
+	postNewTask,
+	getSkillsSubCat,
+} from '../utils/api-createJob';
 
 const CreateJob = (props) => {
-  // do i need to pass category list as props or can i run two useEffect in same page?
+	// do i need to pass category list as props or can i run two useEffect in same page?
 
-  const { categoryList } = props;
-  const [subCategoryList, setSubCategoryList] = useState([]);
-  const [form, setForm] = useState({
-    taskName: "",
-    category: "languages",
-    subCategory: "",
-    description: "",
-    startTime: "",
-    endTimime: "",
-    location: "",
-  });
+	const { categoryList } = props;
+	const [subCategoryList, setSubCategoryList] = useState([]);
+	const [form, setForm] = useState({
+		taskName: '',
+		category: 'languages',
+		subCategory: '',
+		description: '',
+		startTime: '',
+		endTimime: '',
+		location: '',
+	});
 
-  const category = form.category;
+	const category = form.category;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const postedTask = {
-      task_name: form.taskName,
-      booker_id: 1,
-      skill_id: parseInt(form.subCategory),
-      task_description: form.description,
-      start_time: form.startTime,
-      end_time: form.endTime,
-      location: form.location,
-    };
-    console.log(postedTask);
-    postNewTask(postedTask);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const postedTask = {
+			task_name: form.taskName,
+			booker_id: 1,
+			skill_id: parseInt(form.subCategory),
+			task_description: form.description,
+			start_time: form.startTime,
+			end_time: form.endTime,
+			location: form.location,
+		};
+		console.log(postedTask);
+		postNewTask(postedTask);
+	};
 
-  // introduce a state that changes when category selected? To then trigger useEffect?
-  useEffect(() => {
-    getSkillsSubCat(form.category).then((subSkillsFromApi) => {
-      setSubCategoryList(subSkillsFromApi);
-    });
-  }, [form.category]);
+	// introduce a state that changes when category selected? To then trigger useEffect?
+	useEffect(() => {
+		getSkillsSubCat(form.category).then((subSkillsFromApi) => {
+			setSubCategoryList(subSkillsFromApi);
+		});
+	}, [form.category]);
 
-  return (
-    <StyledCreateJob>
-      <form className="CreateJob__form" onSubmit={handleSubmit}>
-        <h2>Post your job</h2>
-        <input
-          id="taskName"
-          type="text"
-          required
-          placeholder="Task Name: "
-          onChange={(e) => setForm({ ...form, taskName: e.target.value })}
-        />
-        <div className="select-container">
-          <select
-            id="category"
-            required
-            placeholder="Category: "
-            onChange={(e) => setForm({ ...form, category: e.target.value })}
-          >
-            <option value="">Please select a category </option>
-            {categoryList.map((category) => {
-              return <option>{category.slug}</option>;
-            })}
-          </select>
-          <select
-            id="subcategory"
-            required
-            placeholder="subCategory: "
-            onChange={(e) => setForm({ ...form, subCategory: e.target.value })}
-          >
-            <option value="">Please select a sub-category </option>
-            {subCategoryList.map((category) => {
-              return (
-                <option value={category.skill_id}>
-                  {category.skill_subcategory}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <input
-          type="text"
-          required
-          placeholder="Description: "
-          onChange={(e) => setForm({ ...form, description: e.target.value })}
-        />
-        <input
-          type="datetime-local"
-          placeholder="Start"
-          onChange={(e) => setForm({ ...form, startTime: e.target.value })}
-        />
-        <input
-          type="datetime-local"
-          placeholder="Finish"
-          onChange={(e) => setForm({ ...form, endTime: e.target.value })}
-        />
-        <input
-          type="text"
-          minLength="3"
-          maxLength="3"
-          required
-          placeholder="Location: "
-          onChange={(e) => setForm({ ...form, location: e.target.value })}
-        />
-        <button className="btn" type="submit">
-          Post new task
-        </button>
-      </form>
-    </StyledCreateJob>
-  );
+	return (
+		<StyledCreateJob>
+			<form className="CreateJob__form" onSubmit={handleSubmit}>
+				<h2>Post your job</h2>
+				<input
+					id="taskName"
+					type="text"
+					required
+					placeholder="Task Name: "
+					onChange={(e) => setForm({ ...form, taskName: e.target.value })}
+				/>
+				<div className="select-container">
+					<select
+						id="category"
+						required
+						placeholder="Category: "
+						onChange={(e) => setForm({ ...form, category: e.target.value })}
+					>
+						<option value="">Please select a category </option>
+						{categoryList.map((category) => {
+							return <option>{category.slug}</option>;
+						})}
+					</select>
+					<select
+						id="subcategory"
+						required
+						placeholder="subCategory: "
+						onChange={(e) => setForm({ ...form, subCategory: e.target.value })}
+					>
+						<option value="">Please select a sub-category </option>
+						{subCategoryList.map((category) => {
+							return (
+								<option value={category.skill_id}>
+									{category.skill_subcategory}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+				<input
+					type="text"
+					required
+					placeholder="Description: "
+					onChange={(e) => setForm({ ...form, description: e.target.value })}
+				/>
+				<input
+					type="datetime-local"
+					placeholder="Start"
+					onChange={(e) => setForm({ ...form, startTime: e.target.value })}
+				/>
+				<input
+					type="datetime-local"
+					placeholder="Finish"
+					onChange={(e) => setForm({ ...form, endTime: e.target.value })}
+				/>
+				<input
+					type="text"
+					minLength="3"
+					maxLength="3"
+					required
+					placeholder="Location: "
+					onChange={(e) => setForm({ ...form, location: e.target.value })}
+				/>
+				<button className="btn" type="submit">
+					Post new task
+				</button>
+			</form>
+		</StyledCreateJob>
+	);
 };
 
 const StyledCreateJob = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-  color: white;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: black;
+	color: white;
 
-  h2 {
-    font-size: 2rem;
-    margin-bottom: 3.5rem;
-  }
+	h2 {
+		font-size: 2rem;
+		margin-bottom: 3.5rem;
+	}
 
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 10rem 10rem;
-    height: 65%;
-    border-radius: 1rem;
-    margin-bottom: 20rem;
-    border: 0.15rem solid #45b480;
-  }
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 10rem 10rem;
+		height: 65%;
+		border-radius: 1rem;
+		margin-bottom: 20rem;
+		border: 0.15rem solid #45b480;
+	}
 
-  input {
-    width: 35rem;
-    margin-bottom: 1rem;
-    padding: 1.5rem 1rem;
-    background-color: black;
-    color: white;
-    border: 0.15rem solid #45b480;
-    border-radius: 0.5rem;
-    font-size: 1rem;
-  }
+	input {
+		width: 35rem;
+		margin-bottom: 1rem;
+		padding: 1.5rem 1rem;
+		background-color: black;
+		color: white;
+		border: 0.15rem solid #45b480;
+		border-radius: 0.5rem;
+		font-size: 1rem;
+	}
 
-  #create {
-    display: inline-block;
-    margin: 0.5rem;
-    margin-bottom: 1rem;
-    font-size: 1.5rem;
-  }
+	#create {
+		display: inline-block;
+		margin: 0.5rem;
+		margin-bottom: 1rem;
+		font-size: 1.5rem;
+	}
 
-  .select-container {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 1rem;
+	.select-container {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 1rem;
 
-    select {
-      padding: 1.3rem 0rem 1.3rem 2rem;
-      margin: 0rem 0.3rem;
-      background-color: black;
-      color: white;
-      border: 0.15rem solid #3ac2bb;
-      border-radius: 0.5rem;
-      width: 48%;
-      font-size: 1rem;
-    }
-  }
+		select {
+			padding: 1.3rem 0rem 1.3rem 2rem;
+			margin: 0rem 0.3rem;
+			background-color: black;
+			color: white;
+			border: 0.15rem solid #3ac2bb;
+			border-radius: 0.5rem;
+			width: 48%;
+			font-size: 1rem;
+		}
+	}
 `;
 
 export default CreateJob;
--- src/components/JobBookingProcess.jsx
+++ src/components/JobBookingProcess.jsx
@@ -5,98 +5,98 @@ import { useAuth } from '../Contexts/AuthContext';
 import { UserContext } from '../Contexts/UserContext';
 
 const JobBookingProcess = () => {
-  const { user } = useContext(UserContext);
-  const { task_id } = useParams();
-  const [task, setTask] = useState({});
-  const [view, setView] = useState('booking view');
-  const { emailVerify } = useAuth();
-  const new_provider = user.user_id;
+	const { user } = useContext(UserContext);
+	const { task_id } = useParams();
+	const [task, setTask] = useState({});
+	const [view, setView] = useState('booking view');
+	const { emailVerify } = useAuth();
+	const new_provider = user.user_id;
 
-  const body = { ...task };
+	const body = { ...task };
 
-  const [err, setErr] = useState(null);
+	const [err, setErr] = useState(null);
 
-  const handleBookingClick = () => {
-    body.provider_id = new_provider;
-    body.task_booking_confirmed = 'true';
+	const handleBookingClick = () => {
+		body.provider_id = new_provider;
+		body.task_booking_confirmed = 'true';
 
-    setErr(null);
-    setView('booked view');
-    emailVerify();
-    patchTaskByID(body, task_id).catch((err) => {
-      setView('booking view');
-      setErr(
-        'Something went wrong and your task could not be booked. Please try again later.'
-      );
-    });
-  };
+		setErr(null);
+		setView('booked view');
+		emailVerify();
+		patchTaskByID(body, task_id).catch((err) => {
+			setView('booking view');
+			setErr(
+				'Something went wrong and your task could not be booked. Please try again later.'
+			);
+		});
+	};
 
-  useEffect(() => {
-    getJobDescription(task_id).then((res) => {
-      setTask(res);
-    });
-  }, [task_id]);
+	useEffect(() => {
+		getJobDescription(task_id).then((res) => {
+			setTask(res);
+		});
+	}, [task_id]);
 
-  if (err) return <p>{err}</p>;
-  return (
-    <div className='jobDescriptionAndBookingPage'>
-      {view === 'booking view' && (
-        <div className='jobDescriptionPage'>
-          <h1>Job Description</h1>
+	if (err) return <p>{err}</p>;
+	return (
+		<div className="jobDescriptionAndBookingPage">
+			{view === 'booking view' && (
+				<div className="jobDescriptionPage">
+					<h1>Job Description</h1>
 
-          <div className='grid-container'>
-            <h2 className='JDcategory'>Category:</h2>
-            <h2 className='JDcategory2'>{task.skill_category}</h2>
+					<div className="grid-container">
+						<h2 className="JDcategory">Category:</h2>
+						<h2 className="JDcategory2">{task.skill_category}</h2>
 
-            <h2 className='JDsubcategory'>Subcategory:</h2>
-            <h2 className='JDsubcategory2'>{task.skill_subcategory}</h2>
+						<h2 className="JDsubcategory">Subcategory:</h2>
+						<h2 className="JDsubcategory2">{task.skill_subcategory}</h2>
 
-            <h2 className='JDlocation'>Location:</h2>
-            <h2 className='JDlocation2'>{task.location}</h2>
+						<h2 className="JDlocation">Location:</h2>
+						<h2 className="JDlocation2">{task.location}</h2>
 
-            <h2 className='JDskillDescription'>Skill:</h2>
-            <h2 className='JDskillDescription2'>{task.skill_description}</h2>
+						<h2 className="JDskillDescription">Skill:</h2>
+						<h2 className="JDskillDescription2">{task.skill_description}</h2>
 
-            <h2 className='JDabout'>About:</h2>
-            <h2 className='JDabout2'>{task.task_description}</h2>
+						<h2 className="JDabout">About:</h2>
+						<h2 className="JDabout2">{task.task_description}</h2>
 
-            <img
-              className='JDtaskThumbnail'
-              src={task.thumbnail_image_url}
-              alt={task.task_description}
-            />
+						<img
+							className="JDtaskThumbnail"
+							src={task.thumbnail_image_url}
+							alt={task.task_description}
+						/>
 
-            <h2 className='JDrequestedBy'>Requested By:</h2>
-            <h2 className='JDrequestedBy2'>
-              {task.first_name} (
-              <a href='https://tmson-api.herokuapp.com/api/users/fthynne0'>
-                {task.username}
-              </a>
-              )
-            </h2>
-          </div>
+						<h2 className="JDrequestedBy">Requested By:</h2>
+						<h2 className="JDrequestedBy2">
+							{task.first_name} (
+							<a href="https://tmson-api.herokuapp.com/api/users/fthynne0">
+								{task.username}
+							</a>
+							)
+						</h2>
+					</div>
 
-          <button className='JDbookTaskButton' onClick={handleBookingClick}>
-            Book
-          </button>
-        </div>
-      )}
+					<button className="JDbookTaskButton" onClick={handleBookingClick}>
+						Book
+					</button>
+				</div>
+			)}
 
-      {view === 'booked view' && (
-        <div>
-          <h1>Congratulations! You booked the task.</h1>
-          <h2>
-            Contact {task.first_name} (
-            <a href='https://tmson-api.herokuapp.com/api/users/fthynne0'>
-              {task.username}
-            </a>
-            )for more information and to confirm your start time and exact
-            location & Check Your Email for a link to confirm your booking.
-          </h2>
-        </div>
-      )}
-    </div>
-  );
+			{view === 'booked view' && (
+				<div>
+					<h1>Congratulations! You booked the task.</h1>
+					<h2>
+						Contact {task.first_name} (
+						<a href="https://tmson-api.herokuapp.com/api/users/fthynne0">
+							{task.username}
+						</a>
+						)for more information and to confirm your start time and exact
+						location & Check Your Email for a link to confirm your booking.
+					</h2>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default JobBookingProcess;
