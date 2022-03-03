d3eda26fc56b2841bdfe183a3706416c213eb1fe
KhalidStonehouse2001 <khalidstonehouse12@outlook.com>
2022-03-03 09:25:37 +0000

    small changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -30,43 +30,37 @@ function Map() {
 	useEffect(() => {
 		getTasks().then((res) => {
 			const postcodesRegx = [];
-			res
-				.forEach((task) => {
-					setTaskId(task.task_id);
+			res.forEach((task) => {
+				setTaskId(task.task_id);
 
-					postcodesRegx.push(
-						task.location.match(
-							/^(GIR 0AA)|((([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) [0-9][A-Z]{2})$/g
-						)
-					);
-					setNewPostcodes(postcodesRegx);
-				})
-				.then(() => {
-					getTasksById(taskId)
-						.then((res) => {
-							setTask({
-								location: res.task.location,
-								task_name: res.task.task_name,
-								task_description: res.task.task_description,
-								skill_category: res.task.skill_category,
-								skill_subcategory: res.task.skill_subcategory,
-								skill_description: res.task.skill_description,
-								thumbnail_image_url: res.task.thumbnail_image_url,
-								username: res.task.username,
-								first_name: res.task.first_name,
-								last_name: res.task.last_name,
-								avatar_url: res.task.avatar_url,
-								email_address: res.task.email_address,
-							});
-						})
-						.then(() => {
-							console.log(newPostcodes);
-							postcodes.lookup(newPostcodes[0]).then((res) => {
-								setLat(res.result[0].result.latitude);
-								setLong(res.result[0].result.longitude);
-							});
-						});
+				postcodesRegx.push(
+					task.location.match(
+						/^(GIR 0AA)|((([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) [0-9][A-Z]{2})$/g
+					)
+				);
+				setNewPostcodes(postcodesRegx);
+				postcodes.lookup(newPostcodes[0]).then((res) => {
+					setLat(res.result[0].result.latitude);
+					setLong(res.result[0].result.longitude);
 				});
+			});
+
+			getTasksById(taskId).then((res) => {
+				setTask({
+					location: res.task.location,
+					task_name: res.task.task_name,
+					task_description: res.task.task_description,
+					skill_category: res.task.skill_category,
+					skill_subcategory: res.task.skill_subcategory,
+					skill_description: res.task.skill_description,
+					thumbnail_image_url: res.task.thumbnail_image_url,
+					username: res.task.username,
+					first_name: res.task.first_name,
+					last_name: res.task.last_name,
+					avatar_url: res.task.avatar_url,
+					email_address: res.task.email_address,
+				});
+			});
 		});
 	}, []);
 
@@ -95,20 +89,6 @@ function Map() {
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				/>
-				<Marker position={[lat, long]} />
-				{/* <Popup position={[lat, long]}>
-					<h4>
-						Name: {task.first_name} {task.last_name}
-					</h4>
-
-					<h4>Location: {task.location}</h4>
-					<h4>Username: {task.username}</h4>
-					<h4>Email: {task.email_address}</h4>
-					<h4>Task Name: {task.task_name}</h4>
-					<h4>Task Info: {task.task_description}</h4>
-					<h4>Category: {task.skill_subcategory}</h4>
-					<h4>Skill Info: {task.skill_description}</h4>
-				</Popup> */}
 			</MapContainer>
 		</div>
 	);
