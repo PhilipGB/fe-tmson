eeb666c1c0b92be93610224f4e4939270cd55f2e
Philip Burgess <philipgrahamburgess@gmail.com>
2022-03-04 12:13:56 +0000

    prettier format

--- src/App.js
+++ src/App.js
@@ -20,55 +20,55 @@ import Map from './components/Map';
 import { getSkills } from '../src/utils/api-createJob';
 
 function App() {
-	const [user, setUser] = useState({
-		user_id: null,
-		username: '',
-		first_name: '',
-		last_name: '',
-		birth_date: '',
-		avatar_url: '',
-		address: '',
-		postcode: '',
-		email_address: '',
-		bio: '',
-	});
-	const location = useLocation();
-	const [categoryList, setCategoryList] = useState([]);
+  const [user, setUser] = useState({
+    user_id: null,
+    username: '',
+    first_name: '',
+    last_name: '',
+    birth_date: '',
+    avatar_url: '',
+    address: '',
+    postcode: '',
+    email_address: '',
+    bio: '',
+  });
+  const location = useLocation();
+  const [categoryList, setCategoryList] = useState([]);
 
-	useEffect(() => {
-		getSkills().then((skillsFromApi) => {
-			setCategoryList(skillsFromApi);
-		});
-	}, []);
+  useEffect(() => {
+    getSkills().then((skillsFromApi) => {
+      setCategoryList(skillsFromApi);
+    });
+  }, []);
 
-	return (
-		<div className="App">
-			<UserContext.Provider value={{ user, setUser }}>
-				<AuthProvider>
-					<Nav />
-					<Routes location={location} key={location.pathname}>
-						<Route element={<PrivateRoutes />}>
-							<Route path="/home" element={<Home />} />
-							<Route path="/map" element={<Map />} />
-							<Route
-								path="/create"
-								element={<CreateJob categoryList={categoryList} />}
-							/>
-							<Route path="/tasks/:task_id" element={<JobBookingProcess />} />
-							<Route path="/profile" element={<Profile />} />
-							<Route path="/profile/:username" element={<UpdateProfile />} />
-						</Route>
-						<Route path="/" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-						<Route path="/forget-password" element={<ForgetPassword />} />
-						<Route path="/tasks" element={<TaskList />} />
-						<Route path="/search-results" element={<SearchResults />} />
-					</Routes>
-					<GlobalStyle />
-				</AuthProvider>
-			</UserContext.Provider>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <UserContext.Provider value={{ user, setUser }}>
+        <AuthProvider>
+          <Nav />
+          <Routes location={location} key={location.pathname}>
+            <Route element={<PrivateRoutes />}>
+              <Route path='/home' element={<Home />} />
+              <Route path='/map' element={<Map />} />
+              <Route
+                path='/create'
+                element={<CreateJob categoryList={categoryList} />}
+              />
+              <Route path='/tasks/:task_id' element={<JobBookingProcess />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/profile/:username' element={<UpdateProfile />} />
+            </Route>
+            <Route path='/' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/forget-password' element={<ForgetPassword />} />
+            <Route path='/tasks' element={<TaskList />} />
+            <Route path='/search-results' element={<SearchResults />} />
+          </Routes>
+          <GlobalStyle />
+        </AuthProvider>
+      </UserContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchResultsPage.jsx
+++ src/components/SearchResultsPage.jsx
@@ -7,64 +7,64 @@ import { StyledJobCard } from '../styles';
 import styled from 'styled-components';
 
 const SearchResults = (props) => {
-	const { search } = useLocation();
-	const [searchResults, setSearchResults] = useState([]);
+  const { search } = useLocation();
+  const [searchResults, setSearchResults] = useState([]);
 
-	console.log(search);
+  console.log(search);
 
-	useEffect(() => {
-		getSearchResults(search).then((searchFromApi) => {
-			setSearchResults(searchFromApi);
-		});
-	}, [search]);
+  useEffect(() => {
+    getSearchResults(search).then((searchFromApi) => {
+      setSearchResults(searchFromApi);
+    });
+  }, [search]);
 
-	return (
-		<StyledResults>
-			<main>
-				<SearchBar />
-				<h1 id="search">Search Results:</h1>
-				<StyledSearchContainer>
-					{searchResults &&
-						searchResults.map((results) => {
-							console.log(results);
-							return (
-								<StyledJobCard key={results.task_id}>
-									<div>
-										<h1>JOB</h1>
-									</div>
-									<img src={results.thumbnail_image_url} />
-									<h2>Search result: {results.task_name}</h2>
-									<h2>Skill category: {results.skill_category}</h2>
-									<h2>Skill subcategory: {results.skill_subcategory}</h2>
-									<Link key={results.task_id} to={`/tasks/${results.task_id}`}>
-										{' '}
-										See more...{' '}
-									</Link>
-								</StyledJobCard>
-							);
-						})}
-				</StyledSearchContainer>
-			</main>
-		</StyledResults>
-	);
+  return (
+    <StyledResults>
+      <main>
+        <SearchBar />
+        <h1 id='search'>Search Results:</h1>
+        <StyledSearchContainer>
+          {searchResults &&
+            searchResults.map((results) => {
+              console.log(results);
+              return (
+                <StyledJobCard key={results.task_id}>
+                  <div>
+                    <h1>JOB</h1>
+                  </div>
+                  <img src={results.thumbnail_image_url} />
+                  <h2>Search result: {results.task_name}</h2>
+                  <h2>Skill category: {results.skill_category}</h2>
+                  <h2>Skill subcategory: {results.skill_subcategory}</h2>
+                  <Link key={results.task_id} to={`/tasks/${results.task_id}`}>
+                    {' '}
+                    See more...{' '}
+                  </Link>
+                </StyledJobCard>
+              );
+            })}
+        </StyledSearchContainer>
+      </main>
+    </StyledResults>
+  );
 };
 
 export const StyledSearchContainer = styled.div`
-	display: grid;
-	width: 70vw;
-	margin: auto;
-	grid-template-columns: repeat(auto-fit, minmax(10rem, 2fr));
+  display: grid;
+  width: 70vw;
+  margin: auto;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 2fr));
 `;
 
 export const StyledResults = styled.div`
-	background-color: black;
-	color: white;
-	height: 100vh;
+  background-color: black;
+  color: white;
+  height: 100vh;
 
-	#search {
-		margin: 2rem 2rem;
-		color: #3ac2bb;
-	}
+  #search {
+    margin: 2rem 2rem;
+    color: #3ac2bb;
+  }
 `;
 
 export default SearchResults;
